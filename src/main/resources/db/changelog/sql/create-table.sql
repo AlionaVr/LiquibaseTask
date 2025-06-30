0b470dbafb6c84038de1b1771ae635a3384b40dc
AlionaVr <alonavershilo@yandex.by>
2025-07-01 00:36:37 +0300

    refactor database schema creation

---
+++ src/main/resources/db/changelog/sql/create-table.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS app;
+
+CREATE TABLE app.CUSTOMERS
+(
+    id           serial PRIMARY KEY,
+    name         VARCHAR(60) NOT NULL,
+    surname      VARCHAR(60) NOT NULL,
+    age          INT         NOT NULL CHECK ( age > 0 and age < 110),
+    phone_number VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE app.ORDERS
+(
+    id           serial PRIMARY KEY,
+    date         DATE DEFAULT CURRENT_DATE,
+    customer_id  INT          NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    amount       INT          NOT NULL CHECK (amount > 0),
+    FOREIGN KEY (customer_id) REFERENCES app.CUSTOMERS (id)
+);
